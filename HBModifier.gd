8a155581dc9006a46a18f2c9ae99a598c7e915a9
Tealnim39 <wagonerjoshual@gmail.com>
2026-01-31 20:04:16 -0700

    Added Leaderboard-Legal support

---
+++ HBModifier.gd
@@ -0,0 +1,44 @@
+class_name HBModifier
+
+# Uses same format as OptionsMenu
+var options = {}
+var modifier_settings: HBSerializable
+var disables_video = false
+var processing_notes = false
+
+func _pre_game(song: HBSong, game: HBRhythmGame):
+	pass
+
+func _post_game(song: HBSong, game: HBRhythmGame):
+	pass
+
+func _preprocess_timing_points(points: Array) -> Array:
+	return points
+
+func _init_plugin():
+	pass
+	
+# Returns the option values to default 
+func reset_defaults():
+	# Dicts are passed by reference so we gotta clone them
+	modifier_settings = get_modifier_settings_class().new()
+
+static func get_modifier_name():
+	return "Modifier"
+
+# Some modifiers might use a different name based on settings
+func get_modifier_list_name():
+	return "Modifier"
+
+static func get_modifier_description():
+	return "Default modifier description, please replace me by overriding static get_modifier_description"
+
+static func get_modifier_settings_class() -> Script:
+	return HBSerializable
+static func get_option_settings() -> Dictionary:
+	return {}
+
+# Override in subclasses - returns true if this modifier is purely cosmetic
+# and doesn't affect gameplay, allowing leaderboard submission
+static func is_leaderboard_legal() -> bool:
+	return false  # Default: modifiers block leaderboards
