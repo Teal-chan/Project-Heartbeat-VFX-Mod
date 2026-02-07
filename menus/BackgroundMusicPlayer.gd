18e8764058f29ec6461e4ea78778f614c3df4dbe
Tealnim39 <wagonerjoshual@gmail.com>
2026-02-07 14:29:48 -0700

    Add files via upload

---
+++ menus/BackgroundMusicPlayer.gd
@@ -0,0 +1,271 @@
+extends Node
+
+class_name HBBackgroundMusicPlayer
+
+signal stream_time_changed
+signal song_started(song)
+
+var song_idx = 0
+var paused := false
+
+class SongPlayer:
+	extends Node
+	
+	const FADE_IN_VOLUME = -70
+	
+	var audio_playback: ShinobuSoundPlayer
+	var voice_audio_playback: ShinobuSoundPlayer
+	
+	var song: HBSong
+	
+	signal song_assets_loaded(assets)
+	signal song_ended()
+	signal stream_time_changed(time)
+	
+	var target_volume := 0.0
+	var volume_offset := 0.0
+	
+	var has_audio_normalization_data = false
+	var song_idx = 0
+	
+	var voice_remap: ShinobuChannelRemapEffect
+	var audio_remap: ShinobuChannelRemapEffect
+	
+	var has_assets := false
+	
+	func _get_song_user_volume(song: HBSong, variant := -1, loudness_offset := 0.0) -> float:
+		var volume_offset := loudness_offset
+		var base_volume := song.get_volume_db()
+		var out_volume := 0.0
+		if song.id in UserSettings.user_settings.per_song_settings:
+			var user_song_settings = UserSettings.user_settings.per_song_settings[song.id] as HBPerSongSettings
+			if variant != -1:
+				volume_offset = song.get_variant_data(variant).get_volume()
+			out_volume = db_to_linear(base_volume + volume_offset) * user_song_settings.volume
+		else:
+			out_volume = db_to_linear(base_volume + volume_offset)
+		return out_volume
+	
+	func load_song_assets():
+		if not song.has_audio() or not song.is_cached():
+			return ERR_FILE_NOT_FOUND
+		var assets_to_load: Array[SongAssetLoader.ASSET_TYPES] = [
+			SongAssetLoader.ASSET_TYPES.AUDIO,
+			SongAssetLoader.ASSET_TYPES.VOICE,
+			SongAssetLoader.ASSET_TYPES.PREVIEW,
+			SongAssetLoader.ASSET_TYPES.BACKGROUND
+		]
+		if not song.has_audio_loudness and not SongDataCache.is_song_audio_loudness_cached(song):
+			assets_to_load.append(SongAssetLoader.ASSET_TYPES.AUDIO_LOUDNESS)
+		var token := SongAssetLoader.request_asset_load(song, assets_to_load)
+		token.assets_loaded.connect(_on_song_assets_loaded)
+		
+		emit_signal("stream_time_changed", song.preview_start / 1000.0)
+		
+		return OK
+	
+	func _init(_song: HBSong, _song_idx: int):
+		song = _song
+		song_idx = _song_idx
+
+	func _ready():
+		set_process(false)
+		
+	func pause():
+		audio_playback.stop()
+		if voice_audio_playback:
+			voice_audio_playback.stop()
+				
+	func resume():
+		audio_playback.start()
+		if voice_audio_playback:
+			voice_audio_playback.start()
+		
+	func _process(_delta):
+		if audio_playback:
+			emit_signal("stream_time_changed", audio_playback.get_playback_position_msec() / 1000.0)
+			var end_time = audio_playback.get_length_msec()
+			if song.preview_end != -1:
+				end_time = song.preview_end
+			if audio_playback.get_playback_position_msec() >= end_time or audio_playback.is_at_stream_end():
+				fade_out()
+				emit_signal("song_ended")
+		
+	func get_song_audio_name():
+		return "%s_%d" % [str(song.id), song_idx]
+	func get_song_voice_audio_name():
+		return "voice_%s_%d" % [str(song.id), song_idx]
+		
+	func _on_song_assets_loaded(token: SongAssetLoader.AssetLoadToken):
+		has_assets = true
+		var audio: SongAssetLoader.SongAudioData = token.get_asset(SongAssetLoader.ASSET_TYPES.AUDIO)
+		var voice: SongAssetLoader.SongAudioData = token.get_asset(SongAssetLoader.ASSET_TYPES.VOICE)
+		var audio_loudness: SongAssetLoader.AudioNormalizationInfo = token.get_asset(SongAssetLoader.ASSET_TYPES.AUDIO_LOUDNESS)
+		if audio and audio.shinobu:
+			if song.has_audio_loudness or SongDataCache.is_song_audio_loudness_cached(song):
+				var song_loudness = 0.0
+				has_audio_normalization_data = true
+				if SongDataCache.is_song_audio_loudness_cached(song):
+					song_loudness = SongDataCache.audio_normalization_cache[song.id].loudness
+				else:
+					song_loudness = song.audio_loudness
+				volume_offset = HBAudioNormalizer.get_offset_from_loudness(song_loudness)
+			elif audio_loudness:
+				volume_offset = HBAudioNormalizer.get_offset_from_loudness(audio_loudness.loudness)
+			
+			target_volume = _get_song_user_volume(song, -1, volume_offset)
+
+			var song_audio_name = get_song_audio_name()
+			var sound_source := audio.shinobu
+			audio_playback = sound_source.instantiate(HBGame.menu_music_group, song.uses_dsc_style_channels())
+			var use_source_channel_count := song.uses_dsc_style_channels() and audio_playback.get_channel_count() >= 4
+			if song.uses_dsc_style_channels() and not use_source_channel_count:
+				audio_playback.queue_free()
+				audio_playback = sound_source.instantiate(HBGame.menu_music_group)
+			add_child(audio_playback)
+			audio_playback.seek(song.preview_start)
+			# Scheduling starts prevents crackles
+			audio_playback.schedule_start_time(Shinobu.get_dsp_time())
+			
+			#audio_playback.connect_sound_to_effect(HBGame.spectrum_analyzer)
+			
+			if voice and voice.shinobu:
+				var song_voice_audio_name = get_song_voice_audio_name()
+				var voice_source := voice.shinobu
+				voice_audio_playback = voice_source.instantiate(HBGame.menu_music_group, use_source_channel_count)
+				voice_audio_playback.seek(song.preview_start)
+				voice_audio_playback.schedule_start_time(Shinobu.get_dsp_time())
+				voice_audio_playback.start()
+				add_child(voice_audio_playback)
+			if song.uses_dsc_style_channels() and audio_playback.get_channel_count() >= 4:
+				if voice_audio_playback:
+					voice_remap = Shinobu.instantiate_channel_remap(voice_audio_playback.get_channel_count(), 2)
+					voice_remap.set_weight(2, 0, 1.0)
+					voice_remap.set_weight(3, 1, 1.0)
+					voice_remap.connect_to_group(HBGame.menu_music_group)
+					voice_audio_playback.connect_sound_to_effect(voice_remap)
+				
+				
+				audio_remap = Shinobu.instantiate_channel_remap(audio_playback.get_channel_count(), 2)
+				audio_remap.set_weight(0, 0, 1.0)
+				audio_remap.set_weight(1, 1, 1.0)
+				audio_remap.connect_to_group(HBGame.menu_music_group)
+				audio_playback.connect_sound_to_effect(audio_remap)
+				
+		if audio_playback:
+			# WARNING: DO NOT SET THE VOLUME FOR THE BACKGORUND MUSIC PLAYER's AUDIO PLAYBACK NODES
+			# OR YOU WILL BE KILLED, AUDIO FADING IS INDEPENDENT FROM VOLUME SO IF YOU DO THAT IT WILL GET
+			# EXPONENTIALLY BOOSTED, KEEP IT AT 1.0 PLEASE
+			audio_playback.fade(500, 0.0, target_volume)
+			audio_playback.start()
+		if voice_audio_playback:
+			voice_audio_playback.fade(500, 0.0, target_volume)
+			voice_audio_playback.start()
+		
+		emit_signal("song_assets_loaded")
+		
+		set_process(true)
+
+	func notify_song_volume_settings_changed():
+		# We are fading out already, do nothing..
+		if not is_processing():
+			return
+		target_volume = _get_song_user_volume(song, -1, volume_offset)
+		if voice_audio_playback:
+			voice_audio_playback.fade(200, -1.0, target_volume)
+		if audio_playback:
+			audio_playback.fade(200, -1.0, target_volume)
+
+	func fade_out():
+		set_process(false) # nothing else should fire while fading out...
+		if audio_playback:
+			audio_playback.fade(500, target_volume, 0.0)
+			if voice_audio_playback:
+				voice_audio_playback.fade(500, target_volume, 0.0)
+		
+		var timer := get_tree().create_timer(0.5, false)
+		timer.timeout.connect(self.queue_free)
+
+var current_song_player: SongPlayer
+
+func play_song(song: HBSong):
+	# Don't start new songs while paused (e.g., media player is playing)
+	if paused:
+		return
+#	if not song.has_audio_loudness and not SongDataCache.is_song_audio_loudness_cached(song):
+#		return
+	if current_song_player:
+		if song == current_song_player.song:
+			return
+	var song_player = SongPlayer.new(song, song_idx)
+	song_idx += 1
+	add_child(song_player)
+	song_player.connect("song_assets_loaded", Callable(self, "_on_song_assets_loaded"))
+	song_player.connect("stream_time_changed", Callable(self, "_on_stream_time_changed"))
+	song_player.connect("song_ended", Callable(self, "_on_song_ended"))
+	
+	if song_player.load_song_assets() == OK:
+		if current_song_player:
+			current_song_player.disconnect("song_assets_loaded", Callable(self, "_on_song_assets_loaded"))
+			current_song_player.disconnect("stream_time_changed", Callable(self, "_on_stream_time_changed"))
+			if not current_song_player.has_assets:
+				current_song_player.queue_free()
+			else:
+				current_song_player.fade_out()
+		current_song_player = song_player
+	else:
+		song_player.queue_free()
+	
+	
+func _on_stream_time_changed(time: float):
+	emit_signal("stream_time_changed", time)
+	
+func _on_song_ended():
+	play_random_song()
+	
+func _on_song_assets_loaded():
+	emit_signal("song_started")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
+		if event.keycode == KEY_F9:
+			play_random_song()
+
+func play_random_song():
+	if SongLoader.songs.size() == 0:
+		return
+	randomize()
+	var found_song: HBSong
+	var attempts := 0
+	const MAX_ATTEMPTS := 40
+	while not found_song and attempts < MAX_ATTEMPTS:
+		var song := SongLoader.songs.values()[randi() % SongLoader.songs.size()] as HBSong
+		var is_current_song = false
+		if current_song_player:
+			is_current_song = current_song_player.song == song
+		if song.has_audio() and song.is_cached() and \
+				(song.has_audio_loudness or SongDataCache.is_song_audio_loudness_cached(song)) and \
+				not is_current_song:
+			found_song = song
+		attempts += 1
+	if found_song:
+		play_song(found_song)
+
+func pause():
+	paused = true
+	if current_song_player:
+		current_song_player.pause()
+
+func resume():
+	paused = false
+	if current_song_player:
+		current_song_player.resume()
+
+func notify_song_volume_settings_changed(song: HBSong):
+	if current_song_player.song == song:
+		current_song_player.notify_song_volume_settings_changed()
+
+func get_current_song_length() -> float:
+	if current_song_player.audio_playback:
+		return current_song_player.audio_playback.get_length_msec() * 0.001
+	return -1.0
---
+++ menus/RandomQuote.gd
@@ -0,0 +1,111 @@
+extends Label
+
+signal birthday_triggered
+
+const QUOTES = [
+	"On the wings of a dream", # TTFAF reference
+	"Dangerous choking hazard!", # RefCounted to plastic instruments
+	"No plastic instruments required!",
+	"Voca- what?", # Vocaloid reference
+	"Ahh, this stuff is really fresh", # Famous sample
+	"Cha-cha now y'all",
+	"Imagine the nerves", # Clone hero Soulless 4 meme
+	"Machinations A", # Soulless 4 meme
+	"NO SHE DID NOT CREATE MINECRAFT YOU MANIAC", # Miku meme
+	"Many hentai games led to this", # Most of EIREXE's previous serious projects were adult games
+	"Mental rhythm therapy", # EIREXE reference
+	"Allô, oui c'est Twingo", # RefCounted to the legendary Renault Twingo
+	"He will attack if you go out through this door", # Twingo meme
+	"Alex's singing passion", # RefCounted to miku's singing passion (a hard song)
+	"Excellent-o", # RefCounted to miku's pronounciation of excellent in PD games
+	"STOP BEING THEM", # RefCounted to daniel ricciardo
+	"You can be my new best friend", # RefCounted
+	"International Turururru Association", # ???
+	"Into the Heartbeat", # WOWOWOWOO YOU BETTER TAKE YOUR CHANCES RIGHT NOW
+	"Stop being them", # Daniel Ricciardo reference
+	"One more time to escape from all this madness", # My spirit will go on by DF
+	"When in doubt, blame blizzin", # Blizzin's math is weird
+	"Your beauty cures all the anxieties in my heart", # Let's see if you can guess who said this
+	"Project Heartbeat V-Spec Nur", # Nissan Skyline R34 reference
+	"The heart is a lie, she plans to burn you", # Portalish reference (also hi xna)
+	"If she saves you, it's likely her name starts with an E", # Sadnesswaifu yosi yosi
+	"Smooooooooth operatooooooor", # RefCounted to Carlos Sainz Junior
+	"Rolling around at the speed of tuturu", # ????
+	"Shoutouts to SimpleFlips",
+	"I'll never break under their madness, I'll be miles above, my head up to the sky", # Ace - I'll never break
+	"Original handcrafted code!", # This code is definitely not stolen
+	"The hate us because they ain't us", # The Interview reference
+	"Now featuring 100% more leaderboards",
+	"Project Project Disappointment", # PPD reference
+	"Project Heartbeat Install from scratch tutorial", # RefCounted to hisokeee's video.
+	"Sé que, si perdo, vosaltres també perdeu", # RefCounted to gohan's bird scene in Catalan,
+	"T'has de enfrontar amb mi. Endevant, mira'm els ulls! No tinc cap por. Estic Segur.", # ditto
+	"Shinoboooo", # Oshino Shinobu reference
+	"Not portable *", # RefCounted to PH not being officially available on portable systems, despite there being a homebrew switch port
+	":v",
+	"The CEO of button mashing", # It is you
+	"Bwoah", # Kimi raikkonen reference
+	"Just leave me alone, I know what to do", # Ditto
+	"Just being a mediocre has never been my ambition. That's not my style.", # Michael Schumacher quote
+	"If God had meant for us to walk, why did he give us fingers that fit game controllers?", # Stirling Moss quote
+	"Oh yeah that was easy, I can't believe it... No it wasn't", # Mika Hakkinen quote
+	"MiyakiWelp MiyakiWelp",
+	"In loving memory of Paçoca the rat, forever in our hearts",
+	"In loving memory of Haru the cat",
+	"In loving memory of Jose Luis, que la tierra te sea leve amigo",
+	"Steam Deck compatible!",
+	"Now featuring a fancy website!",
+	"Always blame lino",
+	"Better on Linux (tm)", # Fuck you micosoft
+	"I'm sorry, I hate to interrupt this. But this is quite honestly the biggest load of limp wristed twaddle I ever heard during my five weeks on television.", # James may quote
+	"Illo Illo Illo", # Illojuan
+	"I really love Rustle's work /that/ floor man, that floor", # RefCounted to hentai artist Rustle
+	"You're winner!", # Big rigs reference
+	"DIVALLER compatible",
+	"Powered by Godot",
+	"Project Heartbeat 72.5", # Gerix17 reference
+	"Made with love, from Spain",
+	"↑ ↑ ↓ ↓ ← → ← → X O",
+	"Tim Sweeney seal of approval", # https://twitter.com/TimSweeneyEpic/status/1521687581712429056
+	"The <redacted> must stay!", # The loli must stay
+	"Uoooooooh",
+	"EIREXE when multiplayer", # EIREXE hates the MP code
+	"Karen bring back the kids this isn't a joke this is a plead for help please please please",
+	"Que tieso", # Belongins reference
+	"Feliz jueves... no es jue... oh es jueves, es jueves...", # Ironmouse reference
+	"Tontoelquelolea", # Typical spanish
+	"Como que 33?", # Fernando Alonso reference
+	"An offer you can't refuse!", # The godfather reference
+	"kyuuonComfy kyuuonComfy", # Kyuuon reference
+	"I really wish I was born in the middle ages", # i really wish i was born in the middle ages so i could legally fuck and marry my sister - Mephisto
+	"Lulu's great eternity", # RIP lulu the dog
+	"We love you Grandpa Luis!", # RIP gramps
+	"Don't ask why they call her Chernobyl", # Never ask Kami why they call her that
+	"LAS GRAPAS DONDE???", # Never ask guinii about that one vtuber
+	"Pick up that can", # HL2 reference
+]
+
+const BIRTHDAYS = {
+	[27, 02]: "EIREXE",
+	[26, 03]: "Project Heartbeat",
+	[23, 04]: "Sadnesswaifu",
+	[04, 05]: "Aiko",
+	[28, 08]: "Alex",
+	[13, 10]: "Kami",
+	[8, 02]: "Yahoo",
+	[31, 03]: "Lino",
+	[30, 05]: "Teal",
+}
+
+func _ready():
+	var date_time = Time.get_datetime_dict_from_system()
+	randomize()
+	if date_time.day == 14 and date_time.month == 2:
+		text = "Love is in the air!"
+	else:
+		text = QUOTES[randi_range(0, QUOTES.size()-1)]
+		for birthday in BIRTHDAYS:
+			if birthday[0] == date_time.day and birthday[1] == date_time.month:
+				emit_signal("birthday_triggered")
+				text = "Happy birthday %s!" % [BIRTHDAYS[birthday]]
+	
