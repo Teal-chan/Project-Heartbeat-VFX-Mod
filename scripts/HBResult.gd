9b24afded0642a588b12a5b871b1889717bf2840
Tealnim39 <wagonerjoshual@gmail.com>
2026-02-17 15:20:38 -0700

    Technical Zone implementation

---
+++ scripts/HBResult.gd
@@ -0,0 +1,102 @@
+# A score result for a played song (usually comes inside a HBGameInfo)
+extends HBSerializable
+
+class_name HBResult
+const MAX_SCORE_FROM_HOLD_BONUS = 0.075
+const MAX_SCORE_FROM_SLIDE_BONUS = 0.075
+## Total score (includes bonus scores)
+var score := 0
+## Score from holds
+var hold_bonus := 0
+## Score from chain slides
+var slide_bonus := 0
+## Score from rush notes
+var rush_bonus := 0
+var rush_hits := 0
+var max_combo := 0
+var total_notes := 0
+var notes_hit := 0
+var max_score := 1
+var failed := false
+var used_cheats := false # if user used autoplaye
+var percentage_penalty := 0.0
+
+var _percentage_graph = []
+var _combo_break_points = []
+var _hit_time_offsets := PackedFloat32Array()
+var _hit_judgements := PackedByteArray()
+var _hit_times := PackedInt32Array()
+var _song_end_time = -1.0
+
+var note_ratings = {
+	HBJudge.JUDGE_RATINGS.WORST: 0,
+	HBJudge.JUDGE_RATINGS.SAD: 0,
+	HBJudge.JUDGE_RATINGS.SAFE: 0,
+	HBJudge.JUDGE_RATINGS.FINE: 0,
+	HBJudge.JUDGE_RATINGS.COOL: 0
+}
+# Wrong notes are notes that are hit on time but with the wrong button
+var wrong_note_ratings = {
+	HBJudge.JUDGE_RATINGS.WORST: 0,
+	HBJudge.JUDGE_RATINGS.SAD: 0,
+	HBJudge.JUDGE_RATINGS.SAFE: 0,
+	HBJudge.JUDGE_RATINGS.FINE: 0,
+	HBJudge.JUDGE_RATINGS.COOL: 0
+}
+
+func _init():
+	serializable_fields += ["score", "max_combo", "total_notes", "notes_hit", "note_ratings",
+	"wrong_note_ratings", "failed", "max_score",
+	"hold_bonus", "slide_bonus", "rush_bonus", "rush_hits", "used_cheats", "percentage_penalty"]
+
+func get_serialized_type():
+	return "Result"
+
+func add_percentage_penalty(amount: float) -> void:
+	percentage_penalty = clamp(percentage_penalty + amount, 0.0, 0.30)
+
+enum RESULT_RATING {
+	FAIL,
+	CHEAP,
+	STANDARD,
+	GREAT,
+	EXCELLENT,
+	PERFECT
+}
+
+func get_percentage() -> float:
+	return get_capped_score() / float(max_score) - percentage_penalty
+# Capped score is the score with the capped hold and slide bonuses
+# used for computing percentage
+func get_capped_score():
+	var base_score = get_base_score()
+	var capped_hold_bonus = clamp(hold_bonus, 0, MAX_SCORE_FROM_HOLD_BONUS*max_score)
+	var capped_slide_bonus = clamp(slide_bonus, 0, MAX_SCORE_FROM_SLIDE_BONUS*max_score)
+	return base_score + capped_hold_bonus + capped_slide_bonus
+	
+func get_base_score() -> float:
+	return score - hold_bonus - slide_bonus - rush_bonus
+	
+func get_result_rating() -> int:
+	if failed:
+		return RESULT_RATING.FAIL
+	
+	# All ratings except perfect and fail are score based
+	
+	var failure = note_ratings[HBJudge.JUDGE_RATINGS.SAD]
+	failure += note_ratings[HBJudge.JUDGE_RATINGS.SAFE]
+	failure += note_ratings[HBJudge.JUDGE_RATINGS.WORST]
+	for r in wrong_note_ratings:
+		failure += wrong_note_ratings[r]
+	if failure <= 0:
+		return RESULT_RATING.PERFECT
+
+	var final_score_ratio = get_percentage()
+	if final_score_ratio >= 0.95:
+		return RESULT_RATING.EXCELLENT
+	elif final_score_ratio >= 0.90:
+		return RESULT_RATING.GREAT
+	elif final_score_ratio >= 0.75:
+		return RESULT_RATING.STANDARD
+	else:
+		return RESULT_RATING.CHEAP
